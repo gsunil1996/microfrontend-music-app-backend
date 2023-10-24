3250d7cda964ebf425b23325be668a3c6a3757ef
gsunil1996 <gajulasunilkumar123@gmail.com>
2023-10-24 21:41:40 +0530

    update

---
+++ config/allowedOrigins.js
@@ -0,0 +1,16 @@
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "http://localhost:3500",
+  "http://localhost:8080",
+  "http://localhost:8081",
+  "http://localhost:8082",
+  "http://localhost:8083",
+  "http://localhost:8084",
+  "http://localhost:8085",
+  "http://localhost:8086",
+  "http://localhost:8087",
+  "https://v6-prototype-react-frontend.vercel.app",
+];
+
+module.exports = allowedOrigins;
