ebd7899336c1fe38f889334f8097ebc046eac90f
gsunil1996 <gajulasunilkumar123@gmail.com>
2023-11-02 20:11:35 +0530

    new urls added

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -11,6 +11,14 @@ const allowedOrigins = [
   "http://localhost:8086",
   "http://localhost:8087",
   "https://v6-prototype-react-frontend.vercel.app",
+  "http://admin-microfrontend.apps.ocp4.pacosta.com",
+  "http://auth-microfrontend.apps.ocp4.pacosta.com",
+  "http://creator-microfrontend.apps.ocp4.pacosta.com",
+  "http://header-microfrontend.apps.ocp4.pacosta.com",
+  "http://home-microfrontend.apps.ocp4.pacosta.com",
+  "http://host-microfrontend.apps.ocp4.pacosta.com",
+  "http://sidemenu-microfrontend.apps.ocp4.pacosta.com",
+  "http://user-microfrontend.apps.ocp4.pacosta.com",
 ];
 
 module.exports = allowedOrigins;
